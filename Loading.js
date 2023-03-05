ea7d2735565cadd1160fb54232597123bacf4c10
LivToAir <ynadell06@gmail.com>
2023-03-05 20:36:04 +0200

    Added Google Analytics Tag

--- Loading.js
+++ Loading.js
@@ -1,2 +1,13 @@
 document.getElementById("Loading_Page").style.visibility = "visible";
 var LoadingMax = 50; //100 for release
+
+//Send Log To API
+let data = {Test: "This Is A Test!"};
+
+fetch("http://localhost:3000/", {
+    method: "POST",
+    headers: {'Content-Type': 'application/json'}, 
+    body: JSON.stringify(data)
+}).then(res => {
+    console.log("Request complete! response:", res);
+});
