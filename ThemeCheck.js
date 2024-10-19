7f4d33262000cfa80847118ef0f9d37ae9e53a0b
LivToAir <ynadell06@gmail.com>
2024-10-19 17:40:59 +0300

    Under Construction

--- ThemeCheck.js
+++
@@ -1,9 +0,0 @@
-var matcher = window.matchMedia('(prefers-color-scheme: dark)');
-matcher.addListener(onUpdate);
-onUpdate();
-
-function onUpdate() {
-    if (matcher.matches) {
-        document.querySelector("link[href='./Photos/Logos/LivToAir_Logo_Dark.png']").href = "./Photos/Logos/LivToAir_Logo_Light.png";
-    }
-}
--- index.js
+++ index.js
@@ -1,113 +1,73 @@
-//remove loading screen
-if(!TestingPerformance){
-    var progress = 0, progressDevideBy = 1 * LoadingMax / 100;
-    var LoadingInterval = setInterval(() => {
-        progress++;
-        document.getElementById("LoadingProfress").style.backgroundImage = `linear-gradient(90deg, white ${progress / progressDevideBy}% ${progress / progressDevideBy}%,  rgba(0, 0, 0, 0.1) ${progress / progressDevideBy}%)`
-        if(progress >= LoadingMax){
-            document.getElementById("Loading_Page").style.visibility = "hidden"
-            clearInterval(LoadingInterval);
-        }
-    }, 10);
-}
+var mousewheel;
+var title = document.getElementById("Title")
+var steps = 1, speed = 500, stepMax = 6;
+var MainWebsite = false;
+
+// window.addEventListener("wheel", event => {
+//     mousewheel = Math.sign(event.deltaY);
+//     if(!MainWebsite){
+//         if(steps + mousewheel <= stepMax  && steps + mousewheel > 0)
+//             steps += mousewheel
+//         document.documentElement.style.setProperty("--TitleFont", Math.pow(steps, 3))
+//         title.style.opacity = 1 - (steps - 1) / (stepMax - 1)
+    
+//         if(steps > 1) {
+//             document.getElementById("ScrollDownText").style.bottom = "-100%"
+//             document.getElementById("cursor").style.opacity = 0
+//             document.getElementById("BackgroundDetailAnim").style.animation = "none"
+//             document.getElementById("BackgroundDetailAnim2").style.animation = "none"
+//             document.getElementById("background").style.animation = "none"
+//             document.getElementById("BackgroundDetailAnim").style.opacity = 0
+//             document.getElementById("BackgroundDetailAnim2").style.opacity = 0
+//             document.getElementById("background").style.opacity = 0
+//         }
+//         else{
+//             document.getElementById("ScrollDownText").style.bottom = "10px"
+//             document.getElementById("cursor").style.opacity = 1
+//             document.getElementById("BackgroundDetailAnim").style.animation = "BackgroundDetailAnim 15s linear infinite"
+//             document.getElementById("BackgroundDetailAnim2").style.animation = "BackgroundDetailAnim2 15s -5s linear infinite"
+//             document.getElementById("BackgroundDetailAnim").style.opacity = 0.05
+//             document.getElementById("BackgroundDetailAnim2").style.opacity = 0.05
+//             document.getElementById("background").style.opacity = 0.07
+//         }
+//     }
+    
+//     if(steps >= stepMax) {
+//         MainWebsite = true;
+//         setTimeout(() => {
+//             window.location.href = '../../Pages/Home/home.html'
+//         }, 700);
+//     }
+// });
 
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-window.onload = function(){
-    var rnd = getRandomInt(3);
-    var video = document.getElementById('Aircraft_Preview');
-    var sources =  document.getElementById('Aircraft_Preview_src');
-    if(rnd == 0) sources.src = "./Anims/XA42_Anim.webm";
-    if(rnd == 1) sources.src = "./Anims/C8_Anim.webm";
-    if(rnd == 2) sources.src = "./Anims/C63_Anim.webm";
-    video.load();
-};
-const VideoController = function(isHovering, videoElement)
-{
-    if(!PerformenceMode){
-        if (isHovering == true)
-        {
-            videoElement.play();
-        }
-        else if (isHovering == false)
-        {
-            videoElement.pause();
-            videoElement.currentTime = 0;
-        }
-    }
-}
+var cursorType = Math.round(Math.random())
+if(cursorType == 1) document.getElementById("cursor").style.backgroundImage = "url(../../Assets/cursorBlack2.png)"
+else document.getElementById("cursor").style.backgroundImage = "url(../../Assets/cursorBlack.png)"
 
-function openPage(pageUrl){
-    window.location.replace(pageUrl);
-}
-
-
-let GmailIcon = document.getElementById("MP_BL_Right_Gmail");
-let GmailBox = document.getElementById("MP_BL_Right_Gmail_Popup");
-GmailIcon.addEventListener("mouseover", function() {
-    GmailBox.style.visibility = "visible"
-    GmailBox.style.animation = "none"
-    GmailBox.style.animation = "Gmail_Popup_Hover 0.2s linear forwards"
-});
-GmailIcon.addEventListener("mouseleave", function() {
-    GmailBox.style.animation = "none"
-    GmailBox.style.animation = "Gmail_Popup_Hover_Exit 0.2s linear forwards"
+var mouseX1, mouseY1, mouseX2, mouseY2
+var mouseFlag = false
+window.addEventListener("mousemove", e => {
+    var x = e.clientX, y = e.clientY;
+    if(mouseFlag) { mouseX1 = x; mouseY1 = y; mouseFlag = false}
     setTimeout(() => {
-        GmailBox.style.visibility = "hidden"
-    }, 200);
-});
+        mouseX2 = x; mouseY2 = y
+        mouseFlag = true
+    }, 100);
+    document.getElementById("background").style.maskPosition = (x - 250) + "px" + " " + (y - 250) + "px"
+    document.getElementById("cursor").style.left = x + "px"
+    document.getElementById("cursor").style.top = y + "px"
+    document.getElementById("cursor").style.rotate = (angle(mouseX2, mouseY2, mouseX1, mouseY1) + 90) + "deg"
 
-//Contact Form
-const btn = document.getElementById('CP_R_IB_SendBtn');
-var isValidForm = false;
-document.getElementById('CP_R_InputsBox')
-    .addEventListener('submit', function(event) {
-    event.preventDefault();
-    btn.value = 'Sending...';
-    const serviceID = 'default_service';
-    const templateID = 'template_r9zp2qn';
+    distanceX = x - window.innerWidth / 2
+    distanceY = y - window.innerHeight / 2
+    if(steps <= 1)
+        document.getElementById("Title").style.translate = "calc(-50% + " + (distanceX / 240) + "px) calc(-50% + " + (distanceY / 160) + "px)"
+})
 
-    var name = document.getElementById("CP_R_Name_Input");
-    var email = document.getElementById("CP_R_Email_Input");
-    var subject = document.getElementById("CP_R_Subject_Input");
-    var message = document.getElementById("CP_R_Message_Input");
-    if(name.value.length >= 2 && email.value.length > 2 && subject.value.length > 5 && message.value.length > 10){
-        console.log("Valid Email")
-        isValidForm = true
-    }
-    else{
-        if(name.value.length < 2){
-            btn.value = 'Name Too Short'
-        }
-        else if(email.value.length <= 2){
-            btn.value = 'Not A Valid Email'
-        }
-        else if(subject.value.length <= 5){
-            btn.value = 'Subject Too Short'
-        }
-        else if(message.value.length <= 10){
-            btn.value = 'Message Too Short'
-        }
-        setTimeout(() => {
-            btn.value = 'Send'
-        }, 2000);
-    }
 
-    if(isValidForm){
-        emailjs.sendForm(serviceID, templateID, this)
-        .then(() => {
-            btn.value = 'Email Sent!';
-            setTimeout(() => {
-                btn.value = 'Send'
-            }, 2000);
-        }, (err) => {
-            btn.value = 'Send';
-            alert(JSON.stringify(err));
-        });
-    }
-    else{
-        // alert("error")
-    }
-});
+
+function angle(x1, y1, x2, y2) {
+    var angleRadians = Math.atan2(y2 - y1, x2 - x1);
+    return angleRadians * (180 / Math.PI);
+}
